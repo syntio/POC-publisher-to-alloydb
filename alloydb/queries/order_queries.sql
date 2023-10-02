0cda42f438549b68ac32ee567cc7a7d6e3453741
Daniel <daniel.sandor@syntio.net>
2023-10-02 08:16:37 +0200

    Edited sql queries

--- alloydb/queries/order_queries.sql
+++ alloydb/queries/order_queries.sql
@@ -1,20 +1,19 @@
--- Number of sales per salesperson for each year
-SELECT o.salesperson_fullname AS sales_person_name, date_part('YEAR', o.lasteditedwhen) AS year_of_sale, count(*) AS number_of_sales
-FROM orders3 o
-GROUP BY salesperson_fullname, date_part('YEAR', lasteditedwhen)
-ORDER BY number_of_sales DESC;
-
 -- Total amount spent by each customer per month in 2020
-SELECT date_part('MONTH', o.lasteditedwhen) AS month_of_year, customer_customername AS customer_name, sum(ol.unitprice) AS total_expenses
+EXPLAIN ANALYZE SELECT date_part('MONTH', o.lasteditedwhen) AS month_of_year, customer_customername AS customer_name, sum(ol.unitprice) AS total_expenses
 FROM orders3 o
 INNER JOIN orderlines3 ol ON o.orderid = ol.orderid
 WHERE date_part('YEAR', o.lasteditedwhen) = 2020
 GROUP BY date_part('MONTH', o.lasteditedwhen), o.customer_customername 
 ORDER BY month_of_year;
 
--- Total amount spent by each customer per year
-SELECT date_part('YEAR', o.lasteditedwhen) AS purchase_year, customer_customername AS customer_name, sum(ol.unitprice) AS total_expenses
+-- Number of sales per salesperson for each year
+EXPLAIN ANALYZE SELECT o.salesperson_fullname AS sales_person_name, date_part('YEAR', o.lasteditedwhen) AS year_of_sale, count(*) AS number_of_sales
 FROM orders3 o
-INNER JOIN orderlines3 ol ON o.orderid = ol.orderid
-GROUP BY date_part('YEAR', o.lasteditedwhen), o.customer_customername 
-ORDER BY purchase_year;
+GROUP BY salesperson_fullname, date_part('YEAR', lasteditedwhen)
+ORDER BY number_of_sales DESC;
+
+-- Most sold products
+EXPLAIN ANALYZE SELECT ol.stockitem_stockitemname AS stock_item, SUM(ol.quantity) AS total_quantity_sold
+FROM orderlines3 AS ol
+GROUP BY ol.stockitem_stockitemname
+ORDER BY total_quantity_sold DESC;
